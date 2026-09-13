b52d85bb12d4b09033c488cc0497305e73292311
elmo36-cell <dazamani36@gmail.com>
2026-09-12 23:40:31 -0400

    Inicio del repositorio de Unit Test

---
+++ DemoBiblioteca/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+
+namespace DemoBiblioteca
+{
+    public abstract class Figura
+    {
+        public abstract double Area();
+        public abstract double Perimetro();
+    }
+    public class Cuadrado : Figura
+    {
+        public double Lado { get; set; }
+        public override double Area()=> Lado * Lado;
+        public override double Perimetro() => Lado * 4;
+    }
+
+    public class Rectangulo : Figura
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+        public override double Area() => Base * Altura;
+        public override double Perimetro() => 2 * (Base + Altura);
+    }
+}
