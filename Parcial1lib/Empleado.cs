ef0f6f69cd6737a5a79199e78c10082f2303c7fb
elmo36-cell <dazamani36@gmail.com>
2026-09-19 11:19:56 -0400

    Parcial1

---
+++ Parcial1lib/Empleado.cs
@@ -0,0 +1,41 @@
+﻿namespace Parcial1lib
+{
+    public abstract class Empleado(string nombre, double salarioBase)
+    {
+        public string Nombre { get; set; } = nombre;
+        public double SalarioBase { get; set; } = salarioBase;
+        public abstract double CalcularSalario();
+
+        public class obtenerInformacion()
+        {
+            public string ObtenerInformacion(Empleado empleado)
+            {
+                return $"Nombre: {empleado.Nombre}, Salario Base: {empleado.SalarioBase}, Salario Total: {empleado.CalcularSalario()}";
+            }
+        }
+        public class calcularSalarioTotal()
+        {
+            public double CalcularSalarioTotal(Empleado empleado)
+            {
+                return empleado.CalcularSalario();
+            }
+        }
+        public class Desarrollador(string nombre, double salarioBase, double bonoProductividad) : Empleado(nombre, salarioBase)
+        {
+            public double BonoProductividad { get; set; } = bonoProductividad;
+            public override double CalcularSalario()
+            {
+                return SalarioBase + BonoProductividad;
+            }
+        }
+        public class Gerente(string nombre, double salarioBase, double comision) : Empleado(nombre, salarioBase)
+        {
+            public double Comision { get; set; } = comision;
+            public override double CalcularSalario()
+            {
+                return SalarioBase + Comision;
+            }
+        }
+
+    } 
+}
---
+++ Parcial1test/TestUnitDesarrollador.cs
@@ -0,0 +1,14 @@
+﻿namespace Parcial1test
+{
+    [TestClass]
+    public sealed class TestUnitDesarrollador
+    {
+        [TestMethod]
+        public void TestDesarrollador1()
+        {
+            var desarrollador = new Parcial1lib.Empleado.Desarrollador("Juan", 1000, 200);
+            var salarioTotal = desarrollador.CalcularSalario();
+            Assert.AreEqual(1200, salarioTotal);
+        }  
+    }
+}
